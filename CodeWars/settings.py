89fb43e21c503b721fd870f2377ca611fcd03ec0
Lakshman S <slakshman664@gmail.com>
2021-07-04 00:03:49 +0530

    staging heroku 7/3/21

--- CodeWars/settings.py
+++ CodeWars/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,5 @@ STATICFILES_DIRS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn CodeWars.wsgi
